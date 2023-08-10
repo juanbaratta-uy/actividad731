80c62d5dbbc367875f484d9b40c365c0f2c70bb8
rociolucas <rociolucaspena2004@gmail.com>
2023-08-10 19:31:09 -0300

    Contraseñas coinciden

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,13 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function verificarContrasena() {
+    let pass1 = document.getElementById("password1").value;
+    let pass2 = document.getElementById("password2").value;
+    if (pass1 == pass2) {
+        return true;
+    } else {
+        return false
+    }
+}
